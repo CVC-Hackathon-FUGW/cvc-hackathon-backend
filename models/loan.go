020582176656f8d142a1a3d88e02554600e8ec4e
Lê Huy Hoàng <lehuyhoang@apples-MacBook-Pro.local>
2023-07-12 14:30:12 +0700

    remove create update at

--- models/loan.go
+++ models/loan.go
@@ -1,20 +1,14 @@
 package models
 
-import (
-	"time"
-)
-
 type Loan struct {
-	LoanId       int       `bson:"loan_id" json:"loan_id"`
-	Lender       string    `bson:"lender" json:"lender"`
-	Borrower     string    `bson:"borrower" json:"borrower"`
-	Amount       int       `bson:"amount" json:"amount"`
-	StartTime    int       `bson:"start_time" json:"start_time"`
-	Duration     int       `bson:"duration" json:"duration"`
-	TokenId      int       `bson:"token_id" json:"token_id"`
-	PoolId       int       `bson:"pool_id" json:"pool_id"`
-	TokenAddress string    `bson:"token_address" json:"token_address"`
-	State        bool      `bson:"state" json:"state"`
-	CreatedAt    time.Time `bson:"created_at" json:"created_at"`
-	UpdatedAt    time.Time `bson:"updated_at" json:"updated_at"`
+	LoanId       int    `bson:"loan_id" json:"loan_id"`
+	Lender       string `bson:"lender" json:"lender"`
+	Borrower     string `bson:"borrower" json:"borrower"`
+	Amount       int    `bson:"amount" json:"amount"`
+	StartTime    int    `bson:"start_time" json:"start_time"`
+	Duration     int    `bson:"duration" json:"duration"`
+	TokenId      int    `bson:"token_id" json:"token_id"`
+	PoolId       int    `bson:"pool_id" json:"pool_id"`
+	TokenAddress string `bson:"token_address" json:"token_address"`
+	State        bool   `bson:"state" json:"state"`
 }
