2dd13284c313f3fa23ebd7f0755c71387be0d36d
Baymaxx <hien.vu@icetea.io>
2023-07-13 02:58:18 +0700

    fix: ngrok

--- main.go
+++ main.go
@@ -127,6 +127,7 @@ func main() {
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		c.Writer.Header().Set("ngrok-skip-browser-warning", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
