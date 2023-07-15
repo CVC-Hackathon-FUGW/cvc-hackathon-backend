6bb5d0c873472026b2ccd6c8d6b4db12c49bab5b
Lê Huy Hoàng <lehuyhoang@apples-MacBook-Pro.local>
2023-07-15 23:56:03 +0700

    fix: connection

--- main.go
+++ main.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
-	"github.com/gin-contrib/cors"
 	"github.com/CVC-Hackathon-FUGW/cvc-hackathon-backend/controllers"
 	"github.com/CVC-Hackathon-FUGW/cvc-hackathon-backend/datastore"
 	"github.com/CVC-Hackathon-FUGW/cvc-hackathon-backend/services"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -58,7 +59,9 @@ func init() {
 
 	ctx = context.TODO()
 
-	mongoconn := options.Client().ApplyURI("mongodb://localhost:27017")
+	uri := os.Getenv("DB_URI")
+
+	mongoconn := options.Client().ApplyURI(uri)
 	mongoclient, err = mongo.Connect(ctx, mongoconn)
 	if err != nil {
 		log.Fatal("error while connecting with mongo", err)
@@ -132,7 +135,6 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
-		
 
 		c.Next()
 	}
