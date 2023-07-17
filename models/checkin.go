30a477bf83b87d15bb755aef59c4f3885e92c0e1
Lê Huy Hoàng <lehuyhoang@apples-MacBook-Pro.local>
2023-07-17 14:25:53 +0700

    feat: checkin

---
+++ models/checkin.go
@@ -0,0 +1,9 @@
+package models
+
+type Checkin struct {
+	CheckinId          int    `json:"checkin_id" bson:"checkin_id"`
+	Wallet             string `json:"wallet" bson:"wallet"`
+	NumberChecked      int    `json:"number_checked" bson:"number_checked"`
+	CurrentTokenAmount int64  `json:"current_token_amount" bson:"current_token_amount"`
+	IsActive           bool   `json:"is_active" bson:"is_active"`
+}
